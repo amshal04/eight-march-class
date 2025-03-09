1d106916bedcbfcdfa698c93476decd4c553af9b
amshal04 <amshal.tariq@gmail.com>
2025-03-09 12:55:04 +0500

    Add files via upload

---
+++ eight march class/app.js
@@ -0,0 +1,74 @@
+console.log(Math.round(num));
+console.log(Math.floor(num));
+
+var num1 = Math.random() *10;
+console.log(Math.round(num1));
+var num = parsentIn(prompt("enter a number"));
+console.log(typeof num.toString());
+var num = Math.random().toFixed(3);
+console.log(Number(num));
+
+
+// new Date() is a js builtin constructor function 
+
+
+var daysArr = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+// var daysArr = ['اتوار', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var date = new Date();
+console.log(date.getTime());
+console.log(daysArr[date.getDay()]);
+console.log(date.setMilliseconds(0, 15));
+
+
+("Sep 18, 2017 15:12:55");
+console.log(specificDate);
+
+var date = new Date();
+date.setFullYear(2020);
+date.setMonth(11);
+console.log(date);
+
+
+// function ka pure synthax hy ye name unique rakhty hn like abc, amshal
+function abc(){
+
+}
+
+function amshal(){
+
+}
+function alishba(){
+
+}
+
+function greeting() {
+ alert("Welcome User!!!");
+}
+greeting();
+greeting();
+greeting();
+greeting();
+
+
+function greeting() {
+ console.log("Welcome User!!!");
+}
+
+greeting();
+greeting();
+greeting();
+greeting();
+greeting();
+
+var num1 = +prompt("enter first number");
+var num2 = +prompt("enter first number");
+console.log(num1 + num2);
+
+function add() {
+ var num1 = 50;
+ var num2 =70;
+ console.log(num1 + num2);
+}
+
+add();
+
